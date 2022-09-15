6bfb9139b75b9b3f86bbe92788a6d9ebe46880ef
damianhunt <dhuntsa@gmail.com>
2022-09-15 23:09:42 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
-const links = [
+
+var links = [
     {
-      label: "Week1 notes",
-      url: "week1/index.html"
+        label: "Week1 Notes",
+        url: "week1/index.html"
+    },
+    {
+        label: "Week2 Notes",
+        url: "week2/index.html"
     }
-  ]
+  ];
+
+  listWeeklyLinks(links,"links");
+
+//create href list
+function listWeeklyLinks(weekLinks, listElementName){
+    let ul = document.getElementById(listElementName);
+    if (ul) {
+
+        // loop through objects
+        weekLinks.forEach(element => {
+            
+        //create anchor with attributes
+        let anchor = document.createElement("a");
+        anchor.innerHTML = element.label;
+        anchor.href = element.url;
+        anchor.target= "_blank";
+
+        //create list item with anchor attached
+        let li = document.createElement("li");
+        li.appendChild(anchor);
+
+        ul.appendChild(li);
+        });
+    }
+}
