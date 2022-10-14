4085c0f4d2620fef26f158c52643910c5e581a00
damianhunt <dhuntsa@gmail.com>
2022-10-14 22:29:41 +0200

    commit

---
+++ week5/teamActivity/main.js
@@ -0,0 +1,8 @@
+import Hikes from "./hikes";
+
+const myHike = new Hikes('hikes');
+//document.getElementById('hikes').addEventListener("load",() => {myHike.showHikeList()});
+window.addEventListener("load",() => {myHike.showHikeList()});
+
+//myHike.showHikeList()
+
