7dcc495db288c8a4f2a82008d36ce06571203b60
damianhunt <dhuntsa@gmail.com>
2022-10-15 14:52:10 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@
 var links = [
     {
         label: "Week1 Notes",
-        url: "/week1/index.html"
+        url: "WDD-330---Web-Frontend-Development-II/week1/index.html"
     },
     {
         label: "Week2 Notes",
-        url: "/week2/index.html"
+        url: "WDD-330---Web-Frontend-Development-II/week2/index.html"
     },
     {
         label: "Week3 Notes",
-        url: "/week3/index.html"
+        url: "WDD-330---Web-Frontend-Development-II/week3/index.html"
     },
     {
         label: "Week4 Notes",
-        url: "/week4/index.html"
+        url: "WDD-330---Web-Frontend-Development-II/week4/index.html"
     },
     {
         label: "Week5 Notes",
-        url: "/week5/index.html"
+        url: "WDD-330---Web-Frontend-Development-II/week5/index.html"
     },
     
   ];
